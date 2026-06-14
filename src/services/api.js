1f13a8c09fbcb8ef9e9253df6014c7762571abb5
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-06-14 02:48:41 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({
-	baseURL: 'http://localhost:5001/api',
-	timeout: 1000,
-})
-
-export default api;
+export default axios.create({
+  baseURL: import.meta.env.VITE_API_URL + '/api'
+});
